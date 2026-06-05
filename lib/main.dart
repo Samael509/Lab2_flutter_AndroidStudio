890ba7fa6007b8ccde94051e7724cade2823508e
Samael <pavel718234@gmail.com>
2026-06-05 14:28:46 +0300

    zadanie2 and zadanie 3 in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,3 +24,33 @@ void main() {
     ),
   );
 }
+
+// задание 2
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Scaffold(
+//       body: Center(
+//         child: Text(
+//           'привет! Уютов Павел.\nя студент группы ИСП-231.',
+//           textAlign: TextAlign.center,
+//           style: TextStyle(fontSize: 24),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+// задание 3
+// void main() {
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: Scaffold(
+//       body: Center(
+//         child: Image.network(
+//           'https://picsum.photos/400/300',
+//         ),
+//       ),
+//     ),
+//   ));
+// }
