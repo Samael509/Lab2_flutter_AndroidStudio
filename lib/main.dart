d87f54e249c5ea7b2c4c4066e66043255d39ffa3
Samael <pavel718234@gmail.com>
2026-06-05 13:43:17 +0300

    Style text: size 32, white color and Set gradient direction top to bottom

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.white,
-              Colors.blue,
-              Colors.red,
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.white, Colors.blue, Colors.red],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Center(
-          child: Text("Hello world!"),
+          child: Center(
+            child: Text(
+              "Hello world!",
+              style: TextStyle(color: Colors.white, fontSize: 32),
+            ),
+          ),
         ),
       ),
     ),
-  ));
+  );
 }
